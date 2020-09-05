db9ae65878700f83510328b14a9be5bb551cd503
Carlos Arango <carango@netdirectautos.com>
2020-09-05 18:50:13 -0500

    Add CORS annotations

--- src/main/java/com/carlosarango/jumbo/storefinder/controller/StoreController.java
+++ src/main/java/com/carlosarango/jumbo/storefinder/controller/StoreController.java
@@ -5,6 +5,7 @@ import com.carlosarango.jumbo.storefinder.service.StoreService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/stores")
 @RequiredArgsConstructor
+@CrossOrigin
 public class StoreController {
 
   private final StoreService storeService;
